2ae7d9b29e73c54add9cfa3c8d5a086516a08242
AlexisNarvaez <alexiszed19@gmail.com>
2019-12-07 20:37:48 -0600

    Modulo GraphQL (Configuración)

---
+++ src/app/graphql/graphql.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
+  ]
+})
+export class GraphqlModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+
+    apollo.create({
+      link: httpLink.create({ uri: 'http://localhost:5300/graphql'}),
+      cache: new InMemoryCache()
+    });
+
+  }
+}
